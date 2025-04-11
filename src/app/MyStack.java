6164b3429474d0b9c654fbcd4b5e1bd07dcbd541
Yerassyl <108489760+yeunikey@users.noreply.github.com>
2025-04-11 18:36:46 +0500

    MyStack.java

---
+++ src/app/MyStack.java
@@ -0,0 +1,35 @@
+package app;
+
+public class MyStack<T> {
+
+    private MyList<T> list = new MyArrayList<>();
+
+    public void push(T item) {
+        list.addLast(item);
+    }
+
+    public T pop() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        T item = list.getLast();
+        list.removeLast();
+        return item;
+    }
+
+    public T peek() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        return list.getLast();
+    }
+
+    public boolean isEmpty() {
+        return list.size() == 0;
+    }
+
+    public int size() {
+        return list.size();
+    }
+
+}
